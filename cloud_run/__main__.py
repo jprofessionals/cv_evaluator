65bd2d2e7ee4e6f7fb2dfa0cf17b70f6076ecbd8
Magnus Axelsson <magnus.a@jpro.no>
2025-02-19 11:41:38 +0100

    Test updating welcome msgs

--- cloud_run/__main__.py
+++ cloud_run/__main__.py
@@ -12,18 +12,6 @@ service_name = "cv-evaluator-cloud-run-service"
 # Create a Google Artifact Registry
 repo_name = "my-docker-repo"
 
-# repo_exists = True
-# try:
-#     repo = gcp.artifactregistry.get_repository(
-#         location=region,
-#         repository_id=repo_name
-#     )
-# except Exception as e:
-#     pulumi.log.info(f"Repository {repo_name} not found: {e}")
-#     repo_exists = False
-
-# if not repo_exists:
-
 repo = gcp.artifactregistry.Repository(
     "docker-repo",
     format="DOCKER",
@@ -38,10 +26,17 @@ docker_registry_url = f"{region}-docker.pkg.dev/{project_id}/{repo_name}"
 
 # Build and push Docker image to Artifact Registry
 image_name = f"{docker_registry_url}/my-app"
+
 image = Image(
     "my-docker-image",
-    build=DockerBuildArgs(context="./app", platform="linux/amd64"),  # Path to the directory containing your Dockerfile
+    build=DockerBuildArgs(
+        # Dir containing Dockerfile
+        context="./app", 
+        platform="linux/amd64"
+    ),  
     image_name=image_name,
+    # Ensure repo exists
+    opts=pulumi.ResourceOptions(depends_on=[repo])  
 )
 
 # Create a dedicated service account for Cloud Run
@@ -61,7 +56,8 @@ service = gcp.cloudrun.Service(
             service_account_name=sa.email,
             containers=[
                 gcp.cloudrun.ServiceTemplateSpecContainerArgs(
-                    image=image.base_image_name,  # Use the built Docker image
+                    # Use the built Docker image
+                    image=image.base_image_name,  
                 )
             ]
         )
