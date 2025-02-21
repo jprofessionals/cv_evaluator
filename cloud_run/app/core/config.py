27a16c693bbfcbf5aa46ea685af162e9d94e0e88
Magnus Axelsson <magnus.a@jpro.no>
2025-02-21 10:58:51 +0100

    Turn off debug logging

--- cloud_run/app/core/config.py
+++ cloud_run/app/core/config.py
@@ -5,7 +5,7 @@ class Settings(BaseSettings):
     """Application settings."""
 
     PROJECT_NAME: str = "CV Evaluator"
-    DEBUG: bool = True
+    DEBUG: bool = False
 
     CV_PARTNER_API_KEY: str
 
